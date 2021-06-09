81c6c4899d6698365e210aa20a20096b6c3bc13d
Lian Aeschlimann <lian.aeschlimann@bbv.ch>
2021-06-09 13:12:57 +0200

    Open Origin opens file dialog

---
+++ Origin creator/Models/MainWindowModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+namespace Origin_creator
+{
+    class MainWindowModel
+    {
+        //Variables
+        private Origin openOrigin;
+
+        //Constructor
+        public MainWindowModel()
+        {
+        }
+
+        //Methods
+        public void OpenOrigin()
+        {
+            FolderBrowserDialog getFolder = new FolderBrowserDialog();
+            getFolder.ShowDialog();
+            this.openOrigin = new Origin(
+                Path.GetFileName(getFolder.SelectedPath), //Returns text after the last '/'
+                Path.GetFileName(getFolder.SelectedPath).Replace(" ", "_").ToLower(), //Converting the folder name because the subfolders shouldn't use spaces or uppercase.
+                getFolder.SelectedPath
+                );
+        }
+
+        public bool IsFolderDataPack()
+        {
+            if (
+                Directory.Exists(this.openOrigin.FolderPath) //TEST To DELETE
+                                                           /*
+                File.Exists(this.openOrigin.FolderPath + "\\pack.mcmeta") && 
+                Directory.Exists(this.openOrigin.FolderPath + $"\\data\\{this.openOrigin.OriginNameCode}\\powers") && 
+                File.Exists(this.openOrigin.FolderPath + "\\data\\origins\\origin_layers\\origins.json") &&
+                File.Exists(this.openOrigin.FolderPath + ("\\data\\{0}\\origins\\{0}", this.openOrigin.OriginNameCode))*/)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void LoadOrigin()
+        {
+
+        }
+    }
+    
+}
--- Origin creator/ViewModels/MainWindowViewModel.cs
+++ Origin creator/ViewModels/MainWindowViewModel.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using MessageBox = System.Windows.MessageBox;
 
 namespace Origin_creator.ViewModels
 {
@@ -13,5 +16,33 @@ namespace Origin_creator.ViewModels
         public ICommand OpenOriginCommand { get; set; }
         public ICommand CreateNewOriginCommand { get; set; }
         public Visibility OriginMenuVisibility { get; set; }
+
+        private MainWindowModel mainWindowModel;
+
+        public MainWindowViewModel()
+        {
+            this.OriginMenuVisibility = Visibility.Hidden;
+            this.OpenOriginCommand = new RelayCommand(this.SelectOriginToOpen, () => true);
+            this.CreateNewOriginCommand = new RelayCommand(() => MessageBox.Show("",""), () => true);
+            this.mainWindowModel = new MainWindowModel();
+        }
+
+        private void SelectOriginToOpen()
+        {
+            this.mainWindowModel.OpenOrigin(); //Dialog to chose folder gets opened.
+            if (!this.mainWindowModel.IsFolderDataPack())
+            {
+                MessageBoxResult result = MessageBox.Show("Folder is not an Origin data pack", "Invalide folder", MessageBoxButton.OKCancel);
+                if (result == MessageBoxResult.OK)
+                { 
+                    this.SelectOriginToOpen();
+                }
+            }
+            else
+            {
+                this.mainWindowModel.LoadOrigin();
+            }
+            
+        }
     }
 }
